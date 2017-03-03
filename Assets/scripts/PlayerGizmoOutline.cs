9039ed4fd1ee3895b524b888652a797606385437
Marcus Guimaraes <mguimaraes42@gmail.com>
2017-03-03 12:37:26 -0500

    added gizmo outline for player

---
+++ Assets/scripts/PlayerGizmoOutline.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGizmoOutline : MonoBehaviour {
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, 1f);
+	}
+}
