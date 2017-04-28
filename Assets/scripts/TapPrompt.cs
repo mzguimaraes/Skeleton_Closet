f4234904768a4c3e63c8350c20402ceee69a3dde
Duy Lam <ddl315@nyu.edu>
2017-04-27 20:26:03 -0400

    created separate scene, added scripts
    created a separate scene to work on, added scripts for timer countdown and tap prompt

---
+++ Assets/scripts/TapPrompt.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapPrompt : MonoBehaviour {
+
+	public bool isTapped = false;
+
+	public TextMesh text;
+	Color textColor;
+
+	PlayerGrab playerGrabScript;
+
+	bool hasPickedUpBone = false;
+
+	//BoneGoalPosition boneGoalScript;
+
+	// Use this for initialization
+	void Start () {
+		textColor = this.GetComponent<MeshRenderer> ().material.color;
+	//	playerGrabScript = GameObject.Find ("PlayerGrab").GetComponent<PlayerGrab> ();
+	//	boneGoalScript = GameObject.Find ("BoneGoalPosition").GetComponent<BoneGoalPosition> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey(KeyCode.Space)) {
+			hasPickedUpBone = true;
+			textColor.a = 0;
+			this.GetComponent<MeshRenderer> ().material.color = textColor;
+		}
+//		else {
+//			textColor.a = 255f;
+//			this.GetComponent<MeshRenderer> ().material.color = textColor;
+//		}
+
+	}
+}
